1925ba812d47780d897930cdc58ac1d71fcee7e5
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2016-11-02 17:27:26 +0100

    made the setup of sources for the debuger more robust

--- setup-dev.sh
+++ setup-dev.sh
@@ -7,6 +7,9 @@ GOEFIS_REMOTE=https://github.com/subugoe/goefis.git
 LIBRECATHOME=./src
 LIBDIR=$LIBRECATHOME/local
 
+#Other Variables
+PERL_VERSION_FILE=$LIBRECATHOME/.perl-version
+
 #Set call to the cpanm script here if guessing doesn't work
 #CPANM=
 if [ -z ${CPANM+x} ]; then
@@ -28,12 +31,34 @@ fi
 
 #Checkout LibreCat
 mkdir -p ${LIBRECATHOME} 
-git clone $LIBRECAT_REMOTE $LIBRECATHOME
+#Check if directory is empty
+if [ "$(ls -A $LIBRECATHOME)" ]; then 
+    echo "Pulling in Changes"
+    cd ${LIBRECATHOME} && git pull
+else
+    git clone $LIBRECAT_REMOTE $LIBRECATHOME
+fi
 
 #Create directory for Perl modules
 mkdir -p $LIBDIR
 
+if [ -f $PERL_VERSION_FILE ]; then
+    if [ "$(perl -version | sed '2,2!d')" -ne "$(cat $PERL_VERSION_FILE)" ]; then
+        echo "Perl version changes, rebuild of modules will be forced"
+	    rm -rf $LIBDIR && mkdir -p $LIBDIR
+    fi
+fi
+
 #Install dependencies to lib dir
 echo "Using cpanm at $CPANM"
+#this can be done faster, if you skip the tests (-n)
 $CPANM -L $LIBDIR --installdeps $LIBRECATHOME
 $CPANM -L $LIBDIR install Devel::Camelcadedb
+$CPANM -L $LIBDIR install Carton
+
+echo "If something fails to install you might need some additional libraries, since some Perl modules aren't selfconrtained! Look at the logs."
+
+#Save Version of Perl, to recompile modules if needed
+echo $(perl -version | sed '2,2!d') > $PERL_VERSION_FILE
+
+
