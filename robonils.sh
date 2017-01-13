93173e1638c9e73f346f1c6d558e0b7b6ebda71c
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2017-01-13 21:22:23 +0100

    The script can now also handle patches

--- robonils.sh
+++ robonils.sh
@@ -7,7 +7,6 @@ GOEFIS_REMOTE=https://github.com/subugoe/goefis.git
 GOEFIS_OLD=goefis-old
 LAYERDIR=$LOCAL_LAYER
 
-
 git clone $GOEFIS_REMOTE $GOEFIS_OLD
 cd $GOEFIS_OLD
 # Git revison of old layer
@@ -21,6 +20,7 @@ cp -r $GOEFIS_OLD/public $LAYERDIR
 # Files
 cp $GOEFIS_OLD/views/partials/footer.tt $LAYERDIR/views
 cat $GOEFIS_OLD/views/partials/copyright.tt >> $LAYERDIR/views/footer.tt
+cat $GOEFIS_OLD/views/base/js_append.tt >> $LAYERDIR/views/footer.tt
 cp  $GOEFIS_OLD/views/partials/topbar.tt  $LAYERDIR/views
 cat  $GOEFIS_OLD/views/partials/menu.tt >> $LAYERDIR/views/topbar.tt
 cat  $GOEFIS_OLD/views/partials/title.tt >> $LAYERDIR/views/topbar.tt
@@ -29,13 +29,16 @@ cp $GOEFIS_OLD/views/frontdoor/tab_* $LAYERDIR/views/publication
 
 rm -rf $GOEFIS_OLD
 
+# Patching
+for file in *.patch
+do
+     (cd  $LIBRECATHOME &&  patch -p1 -b) < "$file"
+done
 
-# 2678bc6f2a71dfb1a1c0c4311a8443dbb6dbfbfd
-
-#cp -r src/views goefis
-
-#cd goefis
-#for file in ../patches/*.patch
-#do
-#    patch -p1 < "$file"
-#done
+#Move Patched files into the layer and restore the originals
+for file in **/*.orig 
+do
+     echo "Moving patched file ${file%.*} to $LAYERDIR"
+     mv "${file%.*}" $LAYERDIR
+     mv "$file" "${file%.*}"
+done
