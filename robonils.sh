8a150be074cdecdca17789725f59b12a8bdce57d
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2017-01-13 19:58:27 +0100

    added a script which pulls old chnges from the old layer to the new one

---
+++ robonils.sh
@@ -0,0 +1,41 @@
+#!env bash
+
+#Repositories
+GOEFIS_REMOTE=https://github.com/subugoe/goefis.git
+
+# Directories
+GOEFIS_OLD=goefis-old
+LAYERDIR=$LOCAL_LAYER
+
+
+git clone $GOEFIS_REMOTE $GOEFIS_OLD
+cd $GOEFIS_OLD
+# Git revison of old layer
+git checkout 2678bc6f2a71dfb1a1c0c4311a8443dbb6dbfbfd
+cd ..
+
+# Directories
+cp -r $GOEFIS_OLD/config $LAYERDIR
+cp -r $GOEFIS_OLD/scss $LAYERDIR
+cp -r $GOEFIS_OLD/public $LAYERDIR
+# Files
+cp $GOEFIS_OLD/views/partials/footer.tt $LAYERDIR/views
+cat $GOEFIS_OLD/views/partials/copyright.tt >> $LAYERDIR/views/footer.tt
+cp  $GOEFIS_OLD/views/partials/topbar.tt  $LAYERDIR/views
+cat  $GOEFIS_OLD/views/partials/menu.tt >> $LAYERDIR/views/topbar.tt
+cat  $GOEFIS_OLD/views/partials/title.tt >> $LAYERDIR/views/topbar.tt
+cat  $GOEFIS_OLD/views/partials/hero.tt >> $LAYERDIR/views/topbar.tt
+cp $GOEFIS_OLD/views/frontdoor/tab_* $LAYERDIR/views/publication
+
+rm -rf $GOEFIS_OLD
+
+
+# 2678bc6f2a71dfb1a1c0c4311a8443dbb6dbfbfd
+
+#cp -r src/views goefis
+
+#cd goefis
+#for file in ../patches/*.patch
+#do
+#    patch -p1 < "$file"
+#done
