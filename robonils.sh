1cd7f8080ba1d369f291ef015eabd43dff30bfea
Christian Mahnke <mahnke@sub.uni-goettingen.de>
2017-01-20 16:10:18 +0100

    commented out the checkout of the old version of the layer

--- robonils.sh
+++ robonils.sh
@@ -10,24 +10,24 @@ LAYERDIR=$LOCAL_LAYER
 git clone $GOEFIS_REMOTE $GOEFIS_OLD
 cd $GOEFIS_OLD
 # Git revison of old layer
-git checkout 2678bc6f2a71dfb1a1c0c4311a8443dbb6dbfbfd
-cd ..
+#git checkout 2678bc6f2a71dfb1a1c0c4311a8443dbb6dbfbfd
+#cd ..
 
 # Directories
-cp -r $GOEFIS_OLD/config $LAYERDIR
-cp -r $GOEFIS_OLD/scss $LAYERDIR
-cp -r $GOEFIS_OLD/public $LAYERDIR
+#cp -r $GOEFIS_OLD/config $LAYERDIR
+#cp -r $GOEFIS_OLD/scss $LAYERDIR
+#cp -r $GOEFIS_OLD/public $LAYERDIR
 # Files
-cp $GOEFIS_OLD/views/partials/footer.tt $LAYERDIR/views
-cat $GOEFIS_OLD/views/partials/copyright.tt >> $LAYERDIR/views/footer.tt
-cat $GOEFIS_OLD/views/base/js_append.tt >> $LAYERDIR/views/footer.tt
-cp  $GOEFIS_OLD/views/partials/topbar.tt  $LAYERDIR/views
-cat  $GOEFIS_OLD/views/partials/menu.tt >> $LAYERDIR/views/topbar.tt
-cat  $GOEFIS_OLD/views/partials/title.tt >> $LAYERDIR/views/topbar.tt
-cat  $GOEFIS_OLD/views/partials/hero.tt >> $LAYERDIR/views/topbar.tt
-cp $GOEFIS_OLD/views/frontdoor/tab_* $LAYERDIR/views/publication
+#cp $GOEFIS_OLD/views/partials/footer.tt $LAYERDIR/views
+#cat $GOEFIS_OLD/views/partials/copyright.tt >> $LAYERDIR/views/footer.tt
+#cat $GOEFIS_OLD/views/base/js_append.tt >> $LAYERDIR/views/footer.tt
+#cp  $GOEFIS_OLD/views/partials/topbar.tt  $LAYERDIR/views
+#cat  $GOEFIS_OLD/views/partials/menu.tt >> $LAYERDIR/views/topbar.tt
+#cat  $GOEFIS_OLD/views/partials/title.tt >> $LAYERDIR/views/topbar.tt
+#cat  $GOEFIS_OLD/views/partials/hero.tt >> $LAYERDIR/views/topbar.tt
+#cp $GOEFIS_OLD/views/frontdoor/tab_* $LAYERDIR/views/publication
 
-rm -rf $GOEFIS_OLD
+#rm -rf $GOEFIS_OLD
 
 # Patching
 for file in *.patch
